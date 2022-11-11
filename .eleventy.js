6bcc3b13072945746449a9b887ea7196f8842ff3
vaggelisdrak <v.drakontaeidis@gmail.com>
2022-11-11 02:09:38 +0200

    fixed admin

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig){
     eleventyConfig.addWatchTarget("./src/css/");
     eleventyConfig.addPassthroughCopy('./src/loading.js');
     eleventyConfig.addPassthroughCopy('./src/assets/images');
+    eleventyConfig.addPassthroughCopy('./src/admin');
     return {
         passthroughFileCopy: true,
         dir: {
